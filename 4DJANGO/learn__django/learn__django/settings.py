2b52a4e774088e3356da0ba6b135e0c39c7ac121
immad ud din <immad.uddin84@gmail.com>
2026-05-11 23:10:19 +0500

    tasks app added

--- 4DJANGO/learn__django/learn__django/settings.py
+++ 4DJANGO/learn__django/learn__django/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+     "tasks__app",
      "leapyear__app",
      'greetings__app',
      'django.contrib.admin',
--- 4DJANGO/learn__django/learn__django/urls.py
+++ 4DJANGO/learn__django/learn__django/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("greetings/", include("greetings__app.urls")),
-    path("isleapyear/", include("leapyear__app.urls"))
+    path("isleapyear/", include("leapyear__app.urls")),
+    path("", include("tasks__app.urls"))
 ]
