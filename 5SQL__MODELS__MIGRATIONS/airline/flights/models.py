75c844e009ce9d1833c8bec6eb325162a689bc32
immad ud din <immad.uddin84@gmail.com>
2026-05-15 18:03:11 +0500

    Airport, Flight, Passenger Models, flights.html, flight.html Added

--- 5SQL__MODELS__MIGRATIONS/airline/flights/models.py
+++ 5SQL__MODELS__MIGRATIONS/airline/flights/models.py
@@ -1,15 +1,14 @@
+
 from django.db import models
 
 # Create your models here.
 
 class Airport(models.Model):
      code = models.CharField(max_length=3)
-     name = models.CharField(max_length=64)
+     city = models.CharField(max_length=64)
 
      def __str__(self):
           return f"{self.city} ({self.code})"
-
-
 
 
 
@@ -20,3 +19,13 @@ class Flight(models.Model):
 
      def __str__(self):
           return f"{self.id}  From {self.origin} To {self.destination}"
+
+
+
+class Passenger(models.Model):
+     first = models.CharField(max_length=64)
+     last = models.CharField(max_length=64)
+     flights = models.ManyToManyField(Flight, blank=True, related_name="passengers")
+
+     def __str__(self):
+          return f"{self.first} {self.last}"
--- 5SQL__MODELS__MIGRATIONS/airline/flights/views.py
+++ 5SQL__MODELS__MIGRATIONS/airline/flights/views.py
@@ -1,7 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Flight, Airport, Passenger
 
 # Create your views here.
 
 def index(request):
-     return HttpResponse("Hello, World!")
+     return render(request, "flights/index.html", {"flights": Flight.objects.all()})
+
+
+def flight(request, flight_id):
+     flight = Flight.objects.get(pk=flight_id)
+     return render(request, "flights/flight.html", {"flight": flight, "passengers": flight.passengers.all()})
